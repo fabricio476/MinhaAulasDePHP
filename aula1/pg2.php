db13e339ba91847aa6144348480479c724ec8520
Fabricio França <fabriciobtcc@gmail.com>
2021-08-23 05:21:26 -0300

    adicionado a pagina dois

---
+++ aula1/pg2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">  <!-- comentario -->
+<head>
+	<meta charset="utf-8">
+	<title>Aula 1 - pagina 2</title>
+</head>
+
+<body>
+   
+   <h1>Página 2</h1> <!-- titulo 1 -->
+   <p><a href="index.html">Voltar para a página inicial</a></p>
+
+    <!-- código PHP embutido -->
+
+    <?php
+     // comando que mostra algo na tela com o PHP (saida de dados)
+     echo "<h1>Teste</h1>";
+     // na saida de dados qualquer comando HTML deve ser enciado com String
+       
+         // \n\t
+       //esse so envia o texto sem comandos HTML
+       echo "Hello, world!"; //sempre colocar ponto e cirgula
+
+    ?>
+
+  
+
+
+</body>
+</html>
