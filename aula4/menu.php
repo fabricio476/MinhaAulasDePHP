bf0cc9a89b60850f31dcbc5d38f3502dce1ed6e7
Fabrício R. de França <fabriciobtcc@gmail.com>
2021-09-27 03:50:43 -0300

    finalizado o exemplo 3 da aula 4

--- aula4/menu.php
+++ aula4/menu.php
@@ -3,8 +3,9 @@
 		<li><a href="index.php">Home</a></li>
 		<li><a href="exp1.php">Exemplo 1</a></li>
 		<li><a href="exp2.php">Exemplo 2</a></li>
-
-
+		<li><a href="exp3.php">Exemplo 3</a></li>
+		<li><a href="exp2.php">Exemplo 4</a></li>
+		<li><a href="exp3.php">Exemplo 5</a></li>
 	</ul>
 
 </nav>
